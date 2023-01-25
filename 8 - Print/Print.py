f72f7a779a7e0f23456538240bffef1c70eb6ad3
SamuelFLM <felipelimaaa23@gmail.com>
2023-01-25 12:23:04 -0300

    Modulo 8

---
+++ 8 - Print/Print.py
@@ -0,0 +1,8 @@
+import pyautogui as bot
+
+#Tira print da tela inteira
+# bot.screenshot("tela.jpg")
+
+# Tira um print da parte da tela
+    #Values region(ponto inicial , distancia direita, distancia baixo)
+bot.screenshot('calculadora.jpg', region=(1367,169, 315, 534))
